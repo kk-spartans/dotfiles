890423c47cd7c2b4d1de7a0f71d5a4d86772ec32
kk-spartans <kcube.jan+github@gmail.com>
2025-12-12 20:36:30 +0530

    did something

--- .chezmoiscripts/run_after-install-packages.ps1
+++ .chezmoiscripts/run_after-install-packages.ps1
@@ -1,2 +1,5 @@
 winget import -i "$env:userprofile/.local/share/chezmoi/.hidden/import.json" --accept-package-agreements --accept-source-agreements
-winget upgrade --all --force --include-unknown --accept-package-agreements --accept-source-agreements
+winget upgrade --all --force --accept-package-agreements --accept-source-agreements
+
+mise install
+mise upgrade --bump
