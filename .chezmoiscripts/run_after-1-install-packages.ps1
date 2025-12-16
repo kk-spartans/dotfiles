456de1bab6a5fe7c3a4f4c17ab56de60afba451b
kk-spartans <kcube.jan+github@gmail.com>
2025-12-16 18:52:13 +0530

    fixed winget

--- .chezmoiscripts/run_after-1-install-packages.ps1
+++ .chezmoiscripts/run_after-1-install-packages.ps1
@@ -1,5 +1,5 @@
-winget import -i "$env:userprofile/.local/share/chezmoi/.hidden/import.json" --accept-package-agreements --accept-source-agreements --silent | Out-Null
-winget upgrade --all --force --accept-package-agreements --accept-source-agreements --silent | Out-Null
+winget import -i "$env:userprofile/.local/share/chezmoi/.hidden/import.json" --accept-package-agreements --accept-source-agreements
+winget upgrade --all --force --accept-package-agreements --accept-source-agreements
 
 mise install
 mise upgrade --bump
