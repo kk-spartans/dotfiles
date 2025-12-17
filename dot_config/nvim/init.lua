c1e18d12c17e8a1f5cc0ed1922c90cceb04a7e5e
kk-spartans <kcube.jan+github@gmail.com>
2025-12-17 16:51:23 +0530

    removed nvim

--- dot_config/nvim/init.lua
+++
@@ -1,4 +0,0 @@
-require("config.lazy")
-require("config.keybinds")
-require("config.options")
-
--- dot_config/nvim/lua/config/color-setup.lua
+++
@@ -1,5 +0,0 @@
-vim.cmd.colorscheme "catppuccin-mocha"
-vim.opt.termguicolors = true
-
-vim.cmd("highlight Normal guibg=NONE guifg=#555555")
-vim.cmd("highlight NormalNC guibg=NONE guifg=#555555")
