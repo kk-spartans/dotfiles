f262cd4cdc2cecc8c678e351f4c8649d10a0fa69
kk-spartans <kcube.jan+github@gmail.com>
2025-12-20 20:10:06 +0530

    switched from ubi to github backend for mise

--- .chezmoiscripts/run_after-1-install-packages.ps1
+++ .chezmoiscripts/run_after-1-install-packages.ps1
@@ -1,9 +1,10 @@
 winget import -i "$env:userprofile/.local/share/chezmoi/.hidden/import.json" --accept-package-agreements --accept-source-agreements
-winget upgrade --all --accept-package-agreements --accept-source-agreements
 
 winget pin add --id Microsoft.VisualStudio.Community --blocking
 winget pin add --id Spotify.Spotify --blocking
 winget pin add --id Buanzo.FFmpegforAudacity --blocking
+
+winget upgrade --all --accept-package-agreements --accept-source-agreements
 
 mise install
 mise upgrade --bump
--- .chezmoiscripts/run_after-3-spicetify.ps1
+++ .chezmoiscripts/run_after-3-spicetify.ps1
@@ -1,3 +1,4 @@
 if (-not (Get-Command spicetify -ErrorAction SilentlyContinue)) {
+    Read-Host "Login to Spotify and click enter"
     iwr -useb https://gist.githubusercontent.com/kk-spartans/56278494b3b9e7be49e44b3e610b2283/raw/df44647e0f1631f15bb9890e0b62dccb2b3e5747/install.ps1 | iex
 }
