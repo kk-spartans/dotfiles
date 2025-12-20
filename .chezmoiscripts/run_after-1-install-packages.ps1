745220db9fa6d4c3b1d49bb4a418352dd9babc31
kk-spartans <kcube.jan+github@gmail.com>
2025-12-20 19:48:48 +0530

    added pinning and ffmpeg for audacity

--- .chezmoiscripts/run_after-1-install-packages.ps1
+++ .chezmoiscripts/run_after-1-install-packages.ps1
@@ -1,5 +1,9 @@
 winget import -i "$env:userprofile/.local/share/chezmoi/.hidden/import.json" --accept-package-agreements --accept-source-agreements
-winget upgrade --all --force --accept-package-agreements --accept-source-agreements
+winget upgrade --all --accept-package-agreements --accept-source-agreements
+
+winget pin add --id Microsoft.VisualStudio.Community --blocking
+winget pin add --id Spotify.Spotify --blocking
+winget pin add --id Buanzo.FFmpegforAudacity --blocking
 
 mise install
 mise upgrade --bump
