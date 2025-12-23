d10528af5b9262bce39dda20bfb2115592cc22a5
kk-spartans <kcube.jan+github@gmail.com>
2025-12-23 21:00:06 +0530

    added mise to PATH

--- .chezmoiscripts/run_after_2-config.ps1
+++ .chezmoiscripts/run_after_2-config.ps1
@@ -1,11 +1,19 @@
-bat cache --build
-komorebic enable-autostart --whkd
-yasbc enable-autostart
+ $gitPath = "$env:localappdata\Programs\Git\usr\bin"
+ $misePath = "$env:localappdata\mise\shims"
+ $userPath = [Environment]::GetEnvironmentVariable("PATH", "User")
+ if ($userPath -notlike "*$gitPath*") {
+     $userPath += ";$gitPath"
+     [Environment]::SetEnvironmentVariable("PATH", $userPath, "User")
+ }
+ if ($userPath -notlike "*$misePath*") {
+     $userPath += ";$misePath"
+     [Environment]::SetEnvironmentVariable("PATH", $userPath, "User")
+ }
 
-$gitPath = "$env:localappdata\Programs\Git\usr\bin"
-$userPath = [Environment]::GetEnvironmentVariable("PATH", "User")
-if ($userPath -notlike "*$gitPath*") {
-    [Environment]::SetEnvironmentVariable("PATH", "$userPath;$gitPath", "User")
-}
+ $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
-[Environment]::SetEnvironmentVariable("EDITOR", "code", "User")
+ [Environment]::SetEnvironmentVariable("EDITOR", "code", "User")
+
+ bat cache --build
+ komorebic enable-autostart --whkd
+ yasbc enable-autostart
