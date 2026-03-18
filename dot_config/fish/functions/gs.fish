e7ef70db3fb133b14dacf041e6ba5d635d701c59
Karthikeyan KK <kcube.jan+github@gmail.com>
2026-03-18 14:03:22 +0530

    fish script was bombing

--- dot_config/fish/functions/gs.fish
+++ dot_config/fish/functions/gs.fish
@@ -1,12 +1,25 @@
 function gs --description "Git add/commit/pull --rebase/push/status"
-    if test (count $argv) -lt 1
-        echo "Usage: gs <commit message>" >&2
-        return 1
+    argparse 'm=' -- $argv
+    or return
+
+    set -l msg wip
+    if set -q _flag_m
+        set msg "$_flag_m"
+    else if test (count $argv) -gt 0
+        set msg (string join ' ' -- $argv)
     end
 
     command git add --all
-    and command git commit -S -m "$argv[1]"
-    and command git pull --rebase
-    and command git push
-    and command git status
+    command git commit -S -m "$msg"
+    if test $status -ne 0
+        return
+    end
+
+    command git pull --rebase
+    if test $status -ne 0
+        return
+    end
+
+    command git push
+    command git status
 end
