8ec06b4c2bf862d5da94ae7f72366ee324c6050e
Karthikeyan KK <kcube.jan+github@gmail.com>
2026-03-19 17:54:50 +0530

    pnpm fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -21,3 +21,10 @@ if status is-interactive
     thefuck --alias | source
 end
 
+
+# pnpm
+set -gx PNPM_HOME "/home/kk-spartans/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
