4aa57c0f06361a3e7b43a2edbe1b572267de596a
Karthikeyan KK <kcube.jan+github@gmail.com>
2026-05-20 21:45:21 +0530

    enable delta git integration

--- modules/programs/cli/git/delta.nix
+++ modules/programs/cli/git/delta.nix
@@ -6,16 +6,20 @@
   ...
 }:
 {
-  programs = { git.settings = { delta = {
-    hyperlinks = true;
-    line-numbers = true;
-    navigate = true;
-    side-by-side = true;
-  }; 
-core.pager = "delta";
-interactive.diffFilter = "delta --color-only";
-  }; 
-delta.enable = true;
+  programs = {
+    git.settings = {
+      delta = {
+        hyperlinks = true;
+        line-numbers = true;
+        navigate = true;
+        side-by-side = true;
+      };
+    };
+
+    delta = {
+      enable = true;
+      enableGitIntegration = true;
+    };
   };
 
   catppuccin.delta.enable = true;
