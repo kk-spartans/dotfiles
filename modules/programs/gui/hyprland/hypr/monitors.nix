f9b295dff9901ac74c3d62af416cc61bd2761ce4
Karthikeyan KK <kcube.jan+github@gmail.com>
2026-05-31 21:55:58 +0530

    fix waybar crash

--- modules/programs/gui/hyprland/hypr/monitors.nix
+++ modules/programs/gui/hyprland/hypr/monitors.nix
@@ -15,16 +15,16 @@
       "SUPER SHIFT, 9, workspace, 5"
       "SUPER SHIFT, 0, workspace, 10"
 
-      "SUPER SHIFT, 1, movetoworkspace, 1"
-      "SUPER SHIFT, 2, movetoworkspace, 6"
-      "SUPER SHIFT, 3, movetoworkspace, 2"
-      "SUPER SHIFT, 4, movetoworkspace, 7"
-      "SUPER SHIFT, 5, movetoworkspace, 3"
-      "SUPER SHIFT, 6, movetoworkspace, 8"
-      "SUPER SHIFT, 7, movetoworkspace, 4"
-      "SUPER SHIFT, 8, movetoworkspace, 9"
-      "SUPER SHIFT, 9, movetoworkspace, 5"
-      "SUPER SHIFT, 0, movetoworkspace, 10"
+      "SUPER, 1, movetoworkspace, 1"
+      "SUPER, 2, movetoworkspace, 6"
+      "SUPER, 3, movetoworkspace, 2"
+      "SUPER, 4, movetoworkspace, 7"
+      "SUPER, 5, movetoworkspace, 3"
+      "SUPER, 6, movetoworkspace, 8"
+      "SUPER, 7, movetoworkspace, 4"
+      "SUPER, 8, movetoworkspace, 9"
+      "SUPER, 9, movetoworkspace, 5"
+      "SUPER, 0, movetoworkspace, 10"
     ];
   };
 }
--- modules/programs/gui/hyprland/waybar/waybar.nix
+++ modules/programs/gui/hyprland/waybar/waybar.nix
@@ -12,7 +12,7 @@
     mode = "createLink";
   };
 
-  wayland.windowManager.hyprland.settings.exec-once = [ "uwsm app -- waybar" ];
+  wayland.windowManager.hyprland.settings.exec-once = [ "waybar -l info" ];
 
   xdg.configFile."waybar/vram.sh" = {
     source = ./vram.sh;
