61dc4ed433d026f50c8d8d9a597f12114bf4bcd6
Karthikeyan KK <kcube.jan+github@gmail.com>
2026-06-20 16:51:21 +0530

    disable hf xet

--- modules/programs/cli/utils/hf.nix
+++ modules/programs/cli/utils/hf.nix
@@ -11,8 +11,9 @@ let
     text = ''
       export HF_XET_HIGH_PERFORMANCE=1
       export HF_XET_NUM_CONCURRENT_RANGE_GETS=32
-      export HF_HUB_ENABLE_HF_TRANSFER=1
-      exec uvx --with hf_transfer hf "$@"
+      # export HF_HUB_ENABLE_HF_TRANSFER=1
+      export HF_HUB_DISABLE_XET=1 # you would be surprised how much faster this is
+      exec uvx --with hf_transfer --with hf-xet hf "$@"
     '';
   };
 in
