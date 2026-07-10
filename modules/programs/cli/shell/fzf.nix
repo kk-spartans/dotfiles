8bc2804a4a67e37dd69bc89aa271fad84f2b0c49
Karthikeyan KK <kcube.jan+github@gmail.com>
2026-07-10 18:34:17 +0530

    remove tmux integration

--- modules/programs/cli/shell/fzf.nix
+++ modules/programs/cli/shell/fzf.nix
@@ -7,7 +7,7 @@
 {
   programs.fzf = {
     enable = true;
-    tmux.enableShellIntegration = true; # i might not use tmux, i dont care
+    # tmux.enableShellIntegration = true; # i might not use tmux, i dont care
   };
 
   catppuccin.fzf.enable = true;
